51b1257a955afdd47656f0a0401d5e6a0193c580
Anton Begehr <a.begehr@gmail.com>
2020-06-08 22:46:35 +0200

    set itterations to 2
    ComE paper states that 2-3 itterations are enough

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     walk_length = 80  # l: length of each walk
     representation_size = 128  # size of the embedding
     num_workers = 10  # number of thread
-    num_iter = 1  # number of overall iteration
+    num_iter = 2  # number of overall iteration
     reg_covar = 0.00001  # regularization coefficient to ensure positive covar
     input_file = 'Dblp'  # name of the input file
     output_file = 'Dblp'  # name of the output file
