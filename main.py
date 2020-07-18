c6ec6376347417679d8c0763edd201e4e9fc4e5c
Anton Begehr <a.begehr@gmail.com>
2020-07-18 16:06:24 +0200

    anim interval is in ms
    https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.animation.ArtistAnimation.html

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
 
         # ### Animation
         if animate:
-            anim = ArtistAnimation(anim_fig, anim_artists, interval=0.5, blit=True, repeat=False)
+            anim = ArtistAnimation(anim_fig, anim_artists, interval=500, blit=True, repeat=False)
             #anim.to_html5_video()
             # export animation as gif:
             # you may need to install "imagemagick" (ex.: brew install imagemagick)
